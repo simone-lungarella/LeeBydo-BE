7ca9b65b337d083c4b014aedb6dd1c946036b7b3
Simone <simone.l1996@hotmail.it>
2021-07-04 19:07:29 +0200

    Initialization of Spring boot application

---
+++ src/main/java/me/asah/leebydo/LeebydoApplication.java
@@ -0,0 +1,13 @@
+package me.asah.leebydo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeebydoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeebydoApplication.class, args);
+	}
+
+}
